dd1e38e767c431d3be73a3cf86856fdc779f10a9
nem0 <javadib67@gmail.com>
2026-07-25 15:26:04 +0330

    fix: Update Redis client initialization to use protocol version 2

--- apps/ingestion/views.py
+++ apps/ingestion/views.py
@@ -25,7 +25,7 @@ _redis_client = None
 def get_redis():
     global _redis_client
     if _redis_client is None:
-        _redis_client = redis.from_url(settings.REDIS_URL)
+        _redis_client = redis.from_url(settings.REDIS_URL, protocol=2)
     return _redis_client
 
 
